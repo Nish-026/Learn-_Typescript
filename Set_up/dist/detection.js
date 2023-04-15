86696dc4ba95d123cde3a1a4172af8e2f4ae98fd
Nish-026 <nishtha.kashyap2608@gmail.com>
2023-04-15 16:26:14 +0530

    Typescript Learning completed

--- Set_up/dist/detection.js
+++ Set_up/dist/detection.js
@@ -59,3 +59,45 @@ function isAdmin(account) {
         console.log(account.isAdmin);
     }
 }
+////////////////////////////////// instanceof Narrowing
+function logValue(x) {
+    if (x instanceof Date) { // instanceof checks whether this object was instance of some or not
+        console.log(x.toUTCString());
+    }
+    else {
+        console.log(x.toUpperCase());
+    }
+}
+function isFish(pet) {
+    return pet.swim !== undefined;
+}
+function getFood(pet) {
+    if (isFish(pet)) {
+        return "fish food";
+    }
+    else {
+        return "bird food";
+    }
+}
+function getTrueshape(shape) {
+    if (shape.kind == "circle") {
+        return Math.PI * shape.radius ** 2;
+    }
+    // return shape.side * shape.side    // this is giving me error coz rectangle is not included in this function so
+}
+function getArea(shape) {
+    switch (shape.kind) {
+        case "circle":
+            return Math.PI * shape.radius ** 2;
+        case "square":
+            return shape.side * shape.side;
+        case "rectangle":
+            return shape.width * shape.height;
+        default:
+            const defaultforShape = shape; // this is called Exhaustiveness checking
+            return defaultforShape;
+    }
+}
+// Exhaustiveness checking
+//The never type is assignable to every type; however, no type is assignable to never (except never itself). This means you can use narrowing and rely on never turning up to do exhaustive checking in a switch statement.
+//For example, adding a default to our getArea function which tries to assign the shape to never will raise when every possible case has not been handled.
--- Set_up/src/detection.ts
+++ Set_up/src/detection.ts
@@ -1,14 +1,14 @@
 
 
-function detectTypes(val: number | string){
-    if(typeof val=="string"){
+function detectTypes(val: number | string) {
+    if (typeof val == "string") {
         return val.toLowerCase()
     }
-    return val+3
+    return val + 3
 }
 
-function provideId(id:string | null){
-    if(!id){
+function provideId(id: string | null) {
+    if (!id) {
         console.log("Please provide ID ");
         return
     }
@@ -17,43 +17,43 @@ function provideId(id:string | null){
 
 function printAll_(strs: string | string[] | null) {
     if (strs && typeof strs === "object") {
-      for (const s of strs) {
-        console.log(s);
-      }
+        for (const s of strs) {
+            console.log(s);
+        }
     } else if (typeof strs === "string") {
-      console.log(strs);
+        console.log(strs);
     }
-  }
+}
 
-  function printAll(strs: string | string[] | null) {   //string[] means string array
+function printAll(strs: string | string[] | null) {   //string[] means string array
 
     if (strs) {
-      if (typeof strs === "object") {
-        for (const s of strs) {
-          console.log(s);
+        if (typeof strs === "object") {
+            for (const s of strs) {
+                console.log(s);
+            }
+        } else if (typeof strs === "string") {
+            console.log(strs);
         }
-      } else if (typeof strs === "string") {
-        console.log(strs);
-      }
     }
-  }
+}
 
 // Equality narrowing
 // TypeScript also uses switch statements and equality checks like ===, !==, ==, and != to narrow types. For example:
 
 function example(x: string | number, y: string | boolean) {
-  if (x === y) {
-    // We can now call any 'string' method on 'x' or 'y'.
-    x.toUpperCase();
-          
-    y.toLowerCase();
-          
-  } else {
-    console.log(x);
-               
-    console.log(y);
-               
-  }
+    if (x === y) {
+        // We can now call any 'string' method on 'x' or 'y'.
+        x.toUpperCase();
+
+        y.toLowerCase();
+
+    } else {
+        console.log(x);
+
+        console.log(y);
+
+    }
 }
 
 // The in operator narrowing
@@ -63,30 +63,107 @@ function example(x: string | number, y: string | boolean) {
 
 type Fish = { swim: () => void };
 type Bird = { fly: () => void };
- 
+
 function move(animal: Fish | Bird) {
-  if ("swim" in animal) {
-    return animal.swim();
-  }
- 
-  return animal.fly();
+    if ("swim" in animal) {
+        return animal.swim();
+    }
+
+    return animal.fly();
 }
 
-interface Admin{
-    name:string,
-    email:string,
+interface Admin {
+    name: string,
+    email: string,
     isAdmin: boolean
 }
 
-interface user{
-    name:string,
-    email:string
+interface user {
+    name: string,
+    email: string
 }
 
-function isAdmin(account: user | Admin){
+function isAdmin(account: user | Admin) {
     // account.isAdmin  // gives error coz isAdmin is not present in user so what we can do is that
 
-    if("isAdmin" in account){
+    if ("isAdmin" in account) {
         console.log(account.isAdmin)
     }
 }
+
+////////////////////////////////// instanceof Narrowing
+
+function logValue(x: Date | string) {
+    if (x instanceof Date) {   // instanceof checks whether this object was instance of some or not
+        console.log(x.toUTCString());
+
+    } else {
+        console.log(x.toUpperCase());
+    }
+}
+
+////////////////////////////// Using type predicates
+
+
+type fish = { swim: () => void };
+type bird = { fly: () => void };
+
+function isFish(pet: fish | bird): pet is fish {   // here we have defined that pet is going to be fish so the functions
+    return (pet as fish).swim !== undefined;
+}
+
+function getFood(pet: fish | Bird) {
+    if (isFish(pet)) {
+        return "fish food"
+    } else {
+        return "bird food"
+    }
+}
+
+////////////////////////////// Discriminated unions
+
+interface Circle {
+    kind: "circle",
+    radius: number
+}
+interface square {
+    kind: "square"
+    side: number
+}
+interface rectangle {
+    kind: "rectangle"
+    width: number,
+    height: number
+}
+
+type Shape = Circle | square | rectangle
+
+function getTrueshape(shape: Shape) {
+    if (shape.kind == "circle") {
+        return Math.PI * shape.radius ** 2
+    }
+    // return shape.side * shape.side    // this is giving me error coz rectangle is not included in this function so
+}
+
+function getArea(shape: Shape) {
+    switch (shape.kind) {
+        case "circle":
+            return Math.PI * shape.radius ** 2
+
+        case "square":
+            return shape.side * shape.side
+
+        case "rectangle":
+            return shape.width * shape.height
+        
+        default:
+            const defaultforShape: never =shape   // this is called Exhaustiveness checking
+            return defaultforShape
+
+    }
+}
+
+// Exhaustiveness checking
+//The never type is assignable to every type; however, no type is assignable to never (except never itself). This means you can use narrowing and rely on never turning up to do exhaustive checking in a switch statement.
+
+//For example, adding a default to our getArea function which tries to assign the shape to never will raise when every possible case has not been handled.
